f2520f8476de6ecf7142c03274771a13bfaaf58a
Plandevida <plandevida@planes.com>
2012-12-08 14:36:16 +0100

    Proyecto en Netbeans. Implementación de los algoritmos que faltaban. La cuenta del tiempo pasa a tiempo.h y tiempo.cpp (no me linca bien no se que mierda de la arquitectura)

---
+++ build/Debug/GNU-MacOSX/_ext/57853592/ordenacion.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-MacOSX/_ext/57853592/ordenacion.o:  \
+ /Volumes/Datos/Estudios/Univerisdad/segundo/Estructura\ de\ datos\ y\ algoritmos/eda/ordenacion.cpp \
+  /Volumes/Datos/Estudios/Univerisdad/segundo/Estructura\ de\ datos\ y\ algoritmos/eda/ordenacion.h
+
+/Volumes/Datos/Estudios/Univerisdad/segundo/Estructura\ de\ datos\ y\ algoritmos/eda/ordenacion.h:
---
+++ build/Debug/GNU-MacOSX/_ext/57853592/tiempo.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-MacOSX/_ext/57853592/tiempo.o:  \
+ /Volumes/Datos/Estudios/Univerisdad/segundo/Estructura\ de\ datos\ y\ algoritmos/eda/tiempo.cpp \
+  /Volumes/Datos/Estudios/Univerisdad/segundo/Estructura\ de\ datos\ y\ algoritmos/eda/tiempo.h
+
+/Volumes/Datos/Estudios/Univerisdad/segundo/Estructura\ de\ datos\ y\ algoritmos/eda/tiempo.h:
